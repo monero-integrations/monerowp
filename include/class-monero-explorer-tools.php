e7530e79df44211e126e08c9f467325afb2939a1
xiphon <xiphon@protonmail.com>
2020-06-19 17:21:54 +0000

    monero-explorer-tools: explorer url - strip trailing slashes

--- include/class-monero-explorer-tools.php
+++ include/class-monero-explorer-tools.php
@@ -19,6 +19,7 @@ class Monero_Explorer_Tools
     public function __construct($testnet = false)
     {
         $this->url = $testnet ? MONERO_GATEWAY_TESTNET_EXPLORER_URL : MONERO_GATEWAY_MAINNET_EXPLORER_URL;
+        $this->url = preg_replace("/\/+$/", "", $this->url);
     }
 
     private function call_api($endpoint)
